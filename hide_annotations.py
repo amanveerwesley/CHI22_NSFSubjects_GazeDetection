80767e752b772c10b80b8309e72a9b039f2246f9
Amanveer Wesley <amanveerwesley@gmail.com>
2022-08-15 03:28:20 -0500

    slight edits to hiding of critical annotations to curb rater bias

--- hide_annotations.py
+++ hide_annotations.py
@@ -1,13 +1,13 @@
-from asyncore import read
-from ntpath import join
-import os
+from os import listdir
 import cv2
 
-lf = os.listfiles()
+p = 'Frames/Frames2000_WithAnnotations/'
 
-p = 'Frames/Test/'
+lf = os.listdir(p)
+
 
 for n in range(0, len(lf)):
     print(n)
     img = cv2.imread(p + lf[n])
-    
+    img = cv2.rectangle(img, (0, 445), (125, 480), (0,0,0), -1)
+    cv2.imwrite('Frames/Frames2000_NoAnnotations/' + lf[n], img)
